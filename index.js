de35501034caac169279d5525c0131b6a26749da
John Zhang <john.shimin.zhang@gmail.com>
2016-02-10 00:32:03 -0800

    set up blank canvas

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 'use strict';
-const C_WIDTH = 200;
-const C_HEIGHT = 200;
+const C_WIDTH = 800;
+const C_HEIGHT = 600;
 // default color of background pixel, RGBA
-const C_DEFAULTBACKGROUND = [200, 200, 55, 255];
+const C_DEFAULTBACKGROUND = [0, 0, 0, 255];
+const RANDOM_COLOR = false;
 
 function randomColorValue() {
   return Math.floor(Math.random() * 255);
 }
 
-function randomPastal () {
+function randomColor() {
   return [randomColorValue(), randomColorValue(), randomColorValue(), 255];
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  initializeContext();
+  initializeContext({ randomColor: RANDOM_COLOR });
 });
 
-function initializeContext() {
+function initializeContext(options) {
   const canvas = document.getElementById('canvas');
   const canvasContext = canvas.getContext('2d');
   const pixelsLength = 4 * C_WIDTH * C_HEIGHT;
-  window.requestAnimationFrame(drawFrame.bind(window, canvasContext, pixelsLength));
+  const use_random =
+  window.requestAnimationFrame(drawFrame.bind(window, canvasContext, pixelsLength, options.randomColor));
 };
 
-function drawFrame(canvasContext, pixelsLength) {
+function drawFrame(canvasContext, pixelsLength, randomColor) {
   const uintc8 = new Uint8ClampedArray(pixelsLength);
   for (let i = 0; i < pixelsLength; i+=4) {
-    let color = randomPastal();
+    const color = randomColor ? randomColor() : C_DEFAULTBACKGROUND;
     uintc8[i] = color[0];
     uintc8[i + 1] = color[1];
     uintc8[i + 2] = color[2];
     uintc8[i + 3] = color[3];
   }
   canvasContext.putImageData(new ImageData(uintc8, C_WIDTH, C_HEIGHT), 0, 0);
-  window.requestAnimationFrame(drawFrame.bind(window, canvasContext, pixelsLength));
+  window.requestAnimationFrame(drawFrame.bind(window, canvasContext, pixelsLength, randomColor));
 }
